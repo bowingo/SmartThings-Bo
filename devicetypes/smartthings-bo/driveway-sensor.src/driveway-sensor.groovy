3230e60bb6a6627aaf872e0b6e87f3d3217db577
bowingo <bowingo@gmail.com>
2019-12-30 18:59:46 -0600

    Update driveway-sensor.groovy

--- devicetypes/smartthings-bo/driveway-sensor.src/driveway-sensor.groovy
+++ devicetypes/smartthings-bo/driveway-sensor.src/driveway-sensor.groovy
@@ -1,5 +1,6 @@
 /**
- *  Copyright 2015 SmartThings
+ *  Copyright 2019 Bo Wingo
+ *  Edited starting with SmartThings' example Z-Wave Door/Window Sensor
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
